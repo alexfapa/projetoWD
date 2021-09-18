77ee4f96e75b8009a29102186f840de99de022c9
Alex <alexfapa32@gmail.com>
2021-09-18 20:31:29 -0300

    Subindo backend

---
+++ backend/src/test/java/com/wd/projeto01/Projeto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wd.projeto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
